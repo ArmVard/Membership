9b221505c158bb37ba76bfbaeb96af6926da842a
AAnnA97 <157473167+AAnnA97@users.noreply.github.com>
2024-04-06 14:52:21 +0400

    bag fixed

--- script/index.js
+++ script/index.js
@@ -42,7 +42,7 @@ document.getElementById('btn').addEventListener("click", function () {
     const username = document.getElementById("name").value;
     const membershipType = document.getElementById("membershipType").value;
     const user = factory.create(username, membershipType);
-    document.getElementById('usersList').style.transform = 'translate(0)'
+    document.getElementById('usersList').style.transform = 'translate(1000px,60px)'
     console.log(user);
     try {
         const newUser = factory.create(username, membershipType);
